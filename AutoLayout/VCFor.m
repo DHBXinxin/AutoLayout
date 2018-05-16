78fedd200a4c7321798d0e76d8deec879c7760ab
DHBXinxin <461132172@qq.com>
2018-05-16 18:18:09 +0800

    了解NSDictionaryOfVariableBindings方法

--- AutoLayout/VCFor.m
+++ AutoLayout/VCFor.m
@@ -44,16 +44,41 @@
     [self.view addSubview:self.blueView];
     self.redView.translatesAutoresizingMaskIntoConstraints = NO;
     self.blueView.translatesAutoresizingMaskIntoConstraints = NO;
-    NSDictionary *views = NSDictionaryOfVariableBindings(self.redView, self.blueView);
-    //与其相对应的是@{@"self.redView": self.redView, @"self.blueView": self.blueView}
-    views = @{@"redView": self.redView, @"blueView": self.blueView};
-    //这句代码探明了、前面的key不是一定要与后面的值完全一样、只要这个key与控件绑定了就可以、与下面的这个定义相同、只要能传值就可以
-    NSDictionary *spaceMetrics = @{@"space": @30};
-    spaceMetrics = @{@"space": @"30"};
-    //所以这样写也是可以识别为30
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-space-[blueView]-space-[redView(==blueView)]-space-|" options:NSLayoutFormatAlignAllTop | NSLayoutFormatAlignAllBottom metrics:spaceMetrics views:views]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[blueView(50)]-space-|" options:0 metrics:spaceMetrics views:views]];
+//    NSDictionary *views = NSDictionaryOfVariableBindings(self.redView, self.blueView);
+//    //与其相对应的是@{@"self.redView": self.redView, @"self.blueView": self.blueView}
+//    views = @{@"redView": self.redView, @"blueView": self.blueView};
+//    //这句代码探明了、前面的key不是一定要与后面的值完全一样、只要这个key与控件绑定了就可以、与下面的这个定义相同、只要能传值就可以
+//    NSDictionary *spaceMetrics = @{@"space": @30};
+//    spaceMetrics = @{@"space": @"30"};
+//    //所以这样写也是可以识别为30
+//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-space-[blueView]-space-[redView(==blueView)]-space-|" options:NSLayoutFormatAlignAllTop | NSLayoutFormatAlignAllBottom metrics:spaceMetrics views:views]];
+//    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[blueView(50)]-space-|" options:0 metrics:spaceMetrics views:views]];
 
+    NSMutableDictionary *views = [NSMutableDictionary dictionary];
+    NSMutableString *hvfl = [NSMutableString string];
+    NSMutableString *vvfl = [NSMutableString string];
+    NSString *firstKey = nil;
+    NSString *lastKey = nil;
+    for (NSInteger i = 0 ; i < 4; i++) {
+        UIView *view = [[UIView alloc]init];
+        NSString *viewKey = [NSString stringWithFormat:@"view%li",(long)i];
+        [views setObject:view forKey:viewKey];
+        if (i == 0) {
+            firstKey = viewKey;
+            [hvfl appendFormat:@"H:|-20-[%@]",viewKey];
+        } else if (i < 3) {
+            [hvfl appendFormat:@"-20-[%@(==%@)]",viewKey, lastKey];
+        } else if (i == 3) {
+            [hvfl appendFormat:@"-20-[%@(==%@)]-20-|",viewKey, lastKey];
+        }
+        view.backgroundColor = [UIColor redColor];
+        [self.view addSubview:view];
+        view.translatesAutoresizingMaskIntoConstraints = NO;
+        lastKey = viewKey;
+    }
+    [vvfl appendFormat:@"V:[%@(50)]-100-|",firstKey];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:hvfl options:NSLayoutFormatAlignAllTop|NSLayoutFormatAlignAllBottom metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:vvfl options:0 metrics:nil views:views]];
 }
 
 - (void)didReceiveMemoryWarning {
